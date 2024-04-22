18eacb4577f45bb952b0a2f8d9468b2bae4c28af
William <willnever97@hotmail.com>
2024-04-22 10:56:22 +0800

    add api test proj

---
+++ TestProjects/Net6Test/ApisTest/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApisTest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Infor")]
+        public string Infor([FromBody] AX ax)
+        {
+            return ax.Name;
+        }
+    }
+
+    public record AX (string Name);
+}
