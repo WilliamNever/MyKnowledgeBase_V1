1ca8f6f8564a5950fcd7cb8b1a48d19bfc308840
William <willnever97@hotmail.com>
2024-05-30 13:53:58 +0800

    updates for HttpSendService add nlog.config to log exception in details add base service for calling HttpSendService easily

---
+++ TestProjects/Net6Test/StandardLibrary/Exceptions/HttpFailedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace StandardLibrary.Exceptions
+{
+    public class HttpFailedException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public HttpFailedException(HttpStatusCode StatusCode, string content) : base(content)
+        {
+            this.StatusCode = StatusCode;
+        }
+    }
+}
---
+++ TestProjects/Net6Test/StandardLibrary/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using StandardLibrary.IServices;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardLibrary.Services
+{
+    public class BaseService<TService>
+    {
+        //protected static TD MapToModel<TE, TD>(TE model, IMapper _mapper) => _mapper.Map<TE, TD>(model);
+        protected static async Task<T> GetFromAPIAsync<T>(string url, IHttpSendService _httpSendService, HttpClient _client)
+        {
+            var rsl = await _httpSendService.SendAndReadFromJsonAsync<T>(
+                async client => { return await client.GetAsync(url); }, _client);
+            return rsl;
+        }
+    }
+}
