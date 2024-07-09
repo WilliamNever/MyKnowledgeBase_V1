de104bff71dbf341ac033350382383f536dfedbf
William <willnever97@hotmail.com>
2024-07-09 09:19:47 +0800

    add host

---
+++ Projects/Fast8Calculting/Fast8Calculting/AppHost.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast8Calculting
+{
+    public static class AppHost
+    {
+        public static IServiceProvider Providers { get; set; } = null!;
+        public static void SetupHost()
+        {
+            var host = ConfigHost();
+            SetupAppProvider(host.Services);
+        }
+
+        public static T? GetSerivce<T>()
+        {
+            return Providers.GetService<T>();
+        }
+        public static IEnumerable<T> GetSerivces<T>()
+        {
+            return Providers.GetServices<T>();
+        }
+
+        private static void SetupAppProvider(IServiceProvider services)
+        {
+            Providers = services;
+        }
+
+        private static IHost ConfigHost()
+        {
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+            var host = new HostBuilder()
+                    .ConfigureHostConfiguration(hostConfig =>
+                    {
+                        hostConfig.AddEnvironmentVariables();
+                    })
+                    .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddEnvironmentVariables(environment);
+                        config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+                        config.AddJsonFile("appsettings.json", false, true);
+                        //config.AddJsonFile($"appsettings.{environment}.json", true, true);
+                    })
+                    .ConfigureLogging((context, logging) =>
+                    {
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                    })
+                    .Build();
+            return host;
+        }
+    }
+}
--- Projects/Fast8Calculting/Fast8Calculting/Program.cs
+++ Projects/Fast8Calculting/Fast8Calculting/Program.cs
@@ -8,6 +8,7 @@ namespace Fast8Calculting
         [STAThread]
         static void Main()
         {
+            AppHost.SetupHost();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
