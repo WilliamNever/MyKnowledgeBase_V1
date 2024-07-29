2cd96b3c3eaaceafdabb3766e2896fd7cb8f4f35
William <willnever97@hotmail.com>
2024-07-29 09:02:31 +0800

    fix the layout issue in different display scale

--- Projects/Fast8Calculting/Fast8Calculting/Forms/uscBasicEasyInput.Designer.cs
+++ Projects/Fast8Calculting/Fast8Calculting/Forms/uscBasicEasyInput.Designer.cs
@@ -37,6 +37,8 @@
             btnCalc = new Button();
             label4 = new Label();
             txtDes = new TextBox();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -93,7 +95,7 @@
             // btnCalc
             // 
             btnCalc.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            btnCalc.Location = new Point(7, 106);
+            btnCalc.Location = new Point(7, 120);
             btnCalc.Name = "btnCalc";
             btnCalc.Size = new Size(75, 23);
             btnCalc.TabIndex = 6;
@@ -104,7 +106,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(230, 7);
+            label4.Location = new Point(232, 7);
             label4.Name = "label4";
             label4.Size = new Size(78, 15);
             label4.TabIndex = 7;
@@ -113,30 +115,40 @@
             // txtDes
             // 
             txtDes.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            txtDes.Location = new Point(230, 25);
+            txtDes.Location = new Point(232, 28);
             txtDes.Multiline = true;
             txtDes.Name = "txtDes";
             txtDes.ScrollBars = ScrollBars.Vertical;
-            txtDes.Size = new Size(366, 97);
+            txtDes.Size = new Size(369, 72);
             txtDes.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnCalc);
+            panel1.Controls.Add(txtChgRate);
+            panel1.Controls.Add(txtDes);
+            panel1.Controls.Add(txtLower);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(txtUpper);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(612, 146);
+            panel1.TabIndex = 9;
             // 
             // UscBasicEasyInput
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(txtDes);
-            Controls.Add(label4);
-            Controls.Add(btnCalc);
-            Controls.Add(txtChgRate);
-            Controls.Add(txtLower);
-            Controls.Add(txtUpper);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(panel1);
             Name = "UscBasicEasyInput";
-            Size = new Size(607, 137);
+            Size = new Size(612, 146);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -150,5 +162,6 @@
         private Button btnCalc;
         private Label label4;
         private TextBox txtDes;
+        private Panel panel1;
     }
 }
