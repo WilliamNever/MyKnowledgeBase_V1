e1d569e9fbc601ccc14550d8254c53eb884a305e
William <willnever97@hotmail.com>
2024-09-04 15:59:20 +0800

    updates

--- Projects/TimerNotificatoin/TimerNotificatoin.Core/Settings/AppSettings.cs
+++ Projects/TimerNotificatoin/TimerNotificatoin.Core/Settings/AppSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace TimerNotificatoin.Core.Settings
 {
     public class AppSettings
     {
-        public string Notifications { get; set; }
+        public string Notifications { get; set; } = null!;
+        public string Templates { get; set; } = null!;
     }
 }
