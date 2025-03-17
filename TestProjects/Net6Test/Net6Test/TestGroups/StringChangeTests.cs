14efe34c7ce8f3e99ef301272a1e9dd9e946cfb4
William <willnever97@hotmail.com>
2025-03-17 15:03:20 +0800

    update a unit test

--- TestProjects/Net6Test/Net6Test/TestGroups/StringChangeTests.cs
+++ TestProjects/Net6Test/Net6Test/TestGroups/StringChangeTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,6 +12,8 @@ namespace Net6Test.TestGroups
     {
         public async static Task UrlParseQueryString_Test()
         {
+            //FileSystem.DeleteFile(@"D:\DriverSpaceMonitor\CDriver_files_2023-06-19_20215.txt", UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+
             var url = "https://xxx.org.inet/xxx/vvv?sid=11&tId=22&fld=HSh[crr]&code=redcode&dType=ntype&domain=ODM";
             //var querystring = new Uri(url).ParseQueryString();
             var query = new Uri(url).Query;
