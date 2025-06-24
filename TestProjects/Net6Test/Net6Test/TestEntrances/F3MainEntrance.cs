a7338dbf7c0532498bf1e8d6ac6b0f8744be51e7
William <willnever97@hotmail.com>
2025-06-24 16:31:50 +0800

    fix conflicts

--- TestProjects/Net6Test/Net6Test/TestEntrances/F3MainEntrance.cs
+++ TestProjects/Net6Test/Net6Test/TestEntrances/F3MainEntrance.cs
@@ -54,7 +54,7 @@ namespace Net6Test.TestEntrances
             //XMLSchemaTest.ReadXMLFile_Test().Wait();
 
             //ThreadTasksTest.Parallel_ForEach_Test().Wait();
-            ThreadTasksTest.ThreadThrowException_Test().Wait();
+            //ThreadTasksTest.ThreadThrowException_Test().Wait();
             ThreadTasksTest.ContinueWith_Test().Wait();
         }
 
