8bd6ca1b224434c475c995d65dd81892ef76f628
William <willnever97@hotmail.com>
2026-02-26 16:37:40 +0800

    add Services factory service

---
+++ TestProjects/Net6Test/StandardLibrary/ScheduledTaskServiceTemplate/Interfaces/IServicesFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace StandardLibrary.ScheduledTaskServiceTemplate.Interfaces
+{
+    public interface IServicesFactory
+    {
+        T GetRequiredService<T>(IServiceProvider ScopedProvider) where T : class;
+        IEnumerable<T> GetRequiredServices<T>(IServiceProvider ScopedProvider) where T : class;
+        IServiceScope RefreshProviderScope();
+    }
+}
---
+++ TestProjects/Net6Test/StandardLibrary/ScheduledTaskServiceTemplate/ServicesFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StandardLibrary.ScheduledTaskServiceTemplate.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace StandardLibrary.ScheduledTaskServiceTemplate
+{
+    public class ServicesFactory: IServicesFactory
+    {
+        private object _lock = new object();
+        
+        private IServiceProvider _serviceProvider;
+        public ServicesFactory(IServiceProvider provider)
+        {
+            _serviceProvider = provider;
+        }
+        public T GetRequiredService<T>(IServiceProvider ScopedProvider) where T : class
+        {
+            return ScopedProvider.GetRequiredService<T>();
+        }
+        public IEnumerable<T> GetRequiredServices<T>(IServiceProvider ScopedProvider) where T : class
+        {
+            return ScopedProvider.GetServices<T>();
+        }
+        public IServiceScope RefreshProviderScope()
+        {
+            lock (_lock)
+            {
+                return _serviceProvider.CreateScope();
+            }
+        }
+    }
+}
