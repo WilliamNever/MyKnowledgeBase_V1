1307065185f6b4658c1cc4b986a4052262d2f263
William <willnever97@hotmail.com>
2026-06-04 14:06:06 +0800

    update Cache management

--- TestProjects/Net6Test/StandardLibrary/IServices/ICacheManage.cs
+++ TestProjects/Net6Test/StandardLibrary/IServices/ICacheManage.cs
@@ -1,6 +1,11 @@
-﻿namespace StandardLibrary.IServices
+﻿using System;
+
+namespace StandardLibrary.IServices
 {
-    public class ICacheManage
+    public interface ICacheManage
     {
+        bool GetFromCache<T>(string key, out T model);
+        void SetAbsoluteExpirationCache<T>(string key, T value, DateTime ExpDateTime);
+        void SetSlidingExpirationCache<T>(string key, T value);
     }
 }
--- TestProjects/Net6Test/StandardLibrary/Services/CacheManageService.cs
+++ TestProjects/Net6Test/StandardLibrary/Services/CacheManageService.cs
@@ -11,6 +11,7 @@ namespace StandardLibrary.Services
         {
             _cache = cache;
         }
+        #region Private methods
         private MemoryCacheEntryOptions CreateSlidingExpirationEntryOption()
         {
             var copt = new MemoryCacheEntryOptions().SetSize(1);
@@ -25,18 +26,38 @@ namespace StandardLibrary.Services
         }
         private string CreateCacheKey<TModel>(string key) => $"{typeof(TModel).Name}_{key}";
 
+        #endregion
 
+        #region Common read and write
+        public void SetSlidingExpirationCache<T>(string key, T value)
+        {
+            _cache.Set(CreateCacheKey<T>(key)
+                , value, CreateSlidingExpirationEntryOption());
+        }
+        public void SetAbsoluteExpirationCache<T>(string key, T value, DateTime ExpDateTime)
+        {
+            _cache.Set(CreateCacheKey<T>(key)
+                , value, CreateAbsoluteExpirationEntryOption(ExpDateTime));
+        }
+        public bool GetFromCache<T>(string key, out T model)
+        {
+            var cachKey = CreateCacheKey<T>(key);
+            return _cache.TryGetValue(cachKey, out model);
+        }
+        #endregion
+
+        #region Examples
         public string GetAPIToken(string key)
         {
-            var cachKey = CreateCacheKey<string>(key);
-            if (!_cache.TryGetValue(cachKey, out string value))
-                value = null;
-            return value;
+            if (!GetFromCache(key, out string rsl))
+                return null;
+            return rsl;
         }
         public void CacheAPIToken(string key, string value)
         {
-            _cache.Set(CreateCacheKey<string>(key), value
-                , CreateAbsoluteExpirationEntryOption(DateTime.Now.AddHours(2)));
+            SetAbsoluteExpirationCache(key, value, DateTime.Now.AddHours(2));
         }
+        #endregion
+
     }
 }
