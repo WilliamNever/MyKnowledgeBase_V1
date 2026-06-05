c2b376a17a5823ff12d75624fdadff1e7dc87a7e
William <willnever97@hotmail.com>
2026-06-05 16:34:56 +0800

    add a new test model

---
+++ TestProjects/Net6Test/Net6Test/Models/TTempModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Net6Test.Models
+{
+    public class TTempModel<T>
+    {
+        public T? Temp { get; set; } = default;
+
+        //var t1 = new TTempModel<int>();   t1.Temp = 0
+        //var t2 = new TTempModel<int?>();  t1.Temp = null
+    }
+}
