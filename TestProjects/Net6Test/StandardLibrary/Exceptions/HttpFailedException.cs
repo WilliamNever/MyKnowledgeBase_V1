08760b3e6dfc69fccb5c29cc70814cd53be99b12
William <willnever97@hotmail.com>
2026-07-16 10:58:32 +0800

    add some comments

--- TestProjects/Net6Test/StandardLibrary/Exceptions/HttpFailedException.cs
+++ TestProjects/Net6Test/StandardLibrary/Exceptions/HttpFailedException.cs
@@ -7,6 +7,9 @@ namespace StandardLibrary.Exceptions
     public class HttpFailedException : Exception
     {
         public HttpStatusCode StatusCode { get; private set; }
+        /// <summary>
+        /// It is not a good idea to keep HttpResponseMessage in the Exception.
+        /// </summary>
         public HttpResponseMessage Response { get; private set; }
         public HttpFailedException(HttpResponseMessage Response, string msg) : base(msg)
         {
