34519ea1d6dd2dcdbc2d3a503485b157a5a5dc4b
William <willnever97@hotmail.com>
2026-07-30 14:14:30 +0800

    add Azure Keyvault Valus Reading

---
+++ TestProjects/Net6Test/Net8Library/KeyVaultValusRead/KeyVaultConfigRead.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Net8Library.KeyVaultValusRead
+{
+    public static class KeyVaultConfigRead
+    {
+        public static KeyVaultValueDictionary GetConfiguration(AzureKeyVaultSetting setting)
+        {
+            KeyVaultValueDictionary rsl = new();
+            IConfigurationBuilder cfg = new ConfigurationBuilder();
+            var certificateCredential = GetCertificateCredential(
+                setting.TenantId,
+                setting.ClientId,
+                setting.CertificateThumbprint);
+            cfg.AddAzureKeyVault(new Uri(setting.KeyVaultUri), certificateCredential);
+            var config = cfg.Build();
+            foreach (var itm in config.AsEnumerable())
+            {
+                rsl.AddOrUpdate(itm.Key, itm.Value);
+            }
+            return rsl;
+        }
+
+        private static ClientCertificateCredential GetCertificateCredential(string tenantId, string clientId, string thumbprint, bool validOnly = false)
+        {
+            using var certificateStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            certificateStore.Open(OpenFlags.ReadOnly);
+            var collection = certificateStore.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly);
+            var certificate = collection.OfType<X509Certificate2>().Single();
+            return new ClientCertificateCredential(tenantId, clientId, certificate);
+        }
+    }
+}
---
+++ TestProjects/Net6Test/Net8Library/KeyVaultValusRead/KeyVaultValueDictionary.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Net8Library.KeyVaultValusRead
+{
+    public class KeyVaultValueDictionary : IOptions<KeyVaultValueDictionary>
+    {
+        private Dictionary<string, string?> _dictionary = new();
+
+        public KeyVaultValueDictionary Value => this;
+        public Dictionary<string, string?> GetBaseDictionary() => _dictionary;
+        public KeyVaultValueDictionary SetBaseDictionary(Dictionary<string, string?> dictionary)
+        {
+            foreach (var itm in dictionary.AsEnumerable())
+            {
+                AddOrUpdate(itm.Key, itm.Value);
+            }
+            return this;
+        }
+        public KeyVaultValueDictionary AddOrUpdate(string key, string? value)
+        {
+            var upperKey = key.ToUpper();
+            if (_dictionary.Keys.Any(k => k == upperKey))
+                _dictionary[upperKey] = value;
+            else
+                _dictionary.Add(upperKey, value);
+            return this;
+        }
+
+        public string? this[string key]
+        {
+            get => TryGet(key, out string? rls) ? rls : null;
+            set => AddOrUpdate(key, value);
+        }
+        public bool ExistsKey(string key)
+        {
+            var upperKey = key.ToUpper();
+            return _dictionary.Keys.Any(k => k == upperKey);
+        }
+        public bool TryGet(string key, out string? value)
+        {
+            var upperKey = key.ToUpper();
+            return _dictionary.TryGetValue(upperKey, out value);
+        }
+
+        public bool Remove(string key)
+        {
+            var upperKey = key.ToUpper();
+            if (_dictionary.Keys.Any(k => k == upperKey))
+                return _dictionary.Remove(upperKey);
+            else
+                return true;
+        }
+        public void Clear() => _dictionary.Clear();
+    }
+}
