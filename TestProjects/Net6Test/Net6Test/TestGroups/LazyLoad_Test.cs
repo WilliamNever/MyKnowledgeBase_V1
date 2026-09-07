caccd3d87f954cb9bd6e64ccd5dbd17f38f688c9
William <willnever97@hotmail.com>
2026-09-07 14:54:40 +0800

    add a test

--- TestProjects/Net6Test/Net6Test/TestGroups/LazyLoad_Test.cs
+++ TestProjects/Net6Test/Net6Test/TestGroups/LazyLoad_Test.cs
@@ -4,7 +4,23 @@
     {
         public async static Task LazyLoad()
         {
-            int idx = 55;
+            var tsks = new List<Lazy<Task<int>>>();
+
+            //var lzyObj = new Lazy<Task<int>>(() => LazyLoadTest(2));
+            //var rsl = await lzyObj.Value;
+            //Console.WriteLine(rsl);
+
+
+            for (int j = 0; j < 5; j++) {
+                var num = 1 << j;
+                tsks.Add(new Lazy<Task<int>>(() => LazyLoadTest(num), LazyThreadSafetyMode.None));
+            }
+
+            var nums = await Task.WhenAll(tsks.Select(t => t.Value));
+        }
+        public async static Task<int> LazyLoadTest(int LimitedNum = -1)
+        {
+            int idx = 1;
             //var lzyObj = new Lazy<Task<int>>(async () =>
             //{
             //    Console.WriteLine($"Enter - {DateTime.Now}");
@@ -15,25 +31,46 @@
             //});
 
             var lzyObj = new LazyObj(() => idx);
-            var lzyClass = new Lazy<CtentObj>();
-            idx = 66;
-            //_ = lzyObj.Value;
-            Console.WriteLine($"Main Start Working - 5sec - {DateTime.Now}");
+            var lzyClass = new Lazy<CtentObj>(LazyThreadSafetyMode.PublicationOnly);
+            idx = 1 << 0;
+            if ((idx & LimitedNum) > 0)
+                _ = lzyObj.Value;
+            Console.WriteLine($"Main Start Working - {LimitedNum} - 5sec - {DateTime.Now}");
             await Task.Delay(5_000);
-            Console.WriteLine($"Main enter delay - 5sec - {DateTime.Now}");
+            Console.WriteLine($"Main enter delay - {LimitedNum} - 5sec - {DateTime.Now}");
             await Task.Delay(5_000);
-            Console.WriteLine($"Main exits delay - 5sec - {DateTime.Now}");
-            idx = 77;
-            var obj = lzyClass.Value;
-            var rsl = await lzyObj.Value.WaitAsync(CancellationToken.None);
-            Console.WriteLine($"Result rsl - {rsl} - {obj.InitialDateTime}");
-            idx = 88;
-            var rsl1 = await lzyObj.Value.WaitAsync(CancellationToken.None);
-            Console.WriteLine($"Result rsl1 - {rsl1} - {obj.InitialDateTime}");
+            Console.WriteLine($"Main exits delay - {LimitedNum} - 5sec - {DateTime.Now}");
+            
+            idx = 1 << 1;
+            if ((idx & LimitedNum) > 0)
+            {
+                _ = lzyClass.Value;
+                _ = lzyObj.Value;
+
+                Console.WriteLine($"Result rsl - {LimitedNum} - " +
+                    $"{await lzyObj.Value.WaitAsync(CancellationToken.None)}  -  {lzyClass.Value.InitialDateTime}");
+            }
+
+            idx = 1 << 2;
+            if ((idx & LimitedNum) > 0)
+            {
+                _ = lzyObj.Value;
+                Console.WriteLine($"Result rsl1 - {LimitedNum} - " +
+                    $"{await lzyObj.Value.WaitAsync(CancellationToken.None)}  -  {lzyClass.Value.InitialDateTime}");
+            }
+
             await Task.Delay(5_000);
-            idx = 99;
-            var rsl2 = await lzyObj.Value.WaitAsync(CancellationToken.None);
-            Console.WriteLine($"Result rsl2 - {rsl2} - {obj.InitialDateTime}");
+            idx = 1 << 3;
+            if ((idx & LimitedNum) > 0)
+            {
+                _ = lzyObj.Value;
+                Console.WriteLine($"Result rsl2 - {LimitedNum} - " +
+                    $"{await lzyObj.Value.WaitAsync(CancellationToken.None)}  -  {lzyClass.Value.InitialDateTime}");
+            }
+
+            Console.WriteLine($"Result rsl2 - {LimitedNum} - " +
+                    $"{await lzyObj.Value.WaitAsync(CancellationToken.None)}  -  {lzyClass.Value.InitialDateTime}");
+            return LimitedNum;
         }
     }
 
@@ -43,10 +80,10 @@
         {
             lzyObj = new Lazy<Task<int>>(async () =>
             {
-                Console.WriteLine($"Enter - {DateTime.Now}");
+                Console.WriteLine($"Enter {idx()} - {DateTime.Now}");
                 await Task.Delay(2_000);
 
-                Console.WriteLine($"Exit - {DateTime.Now}");
+                Console.WriteLine($"Exit {idx()} - {DateTime.Now}");
                 return idx();
             });
         }
