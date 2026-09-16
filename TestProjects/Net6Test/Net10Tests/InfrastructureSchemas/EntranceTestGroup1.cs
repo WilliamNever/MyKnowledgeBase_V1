0be8150248a6f13ab02ffb1015fec01bcd740ee2
William <willnever97@hotmail.com>
2026-09-16 14:42:39 +0800

    Create test groups schema for .net10

---
+++ TestProjects/Net6Test/Net10Tests/InfrastructureSchemas/EntranceTestGroup1.cs
@@ -0,0 +1,10 @@
+﻿namespace Net10Tests.InfrastructureSchemas
+{
+    public class EntranceTestGroup1: EntranceBase
+    {
+        public override async Task MainRunAsync()
+        {
+            Console.WriteLine($"Enter MainRunAsync - {DateTime.Now}");
+        }
+    }
+}
---
+++ TestProjects/Net6Test/Net10Tests/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+
+
+
+
+
+
+using Net10Tests.InfrastructureSchemas;
+
+EntranceBase main
+= new EntranceTestGroup1();
+
+await main.MainRunAsync();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#region End Part
+
+Action EndInfor = () =>
+{
+    Console.WriteLine();
+    Console.WriteLine($"Any key to exit......");
+    while (true)
+    {
+        var readKey = Console.ReadKey();
+        if (readKey.Key == ConsoleKey.Enter) break;
+        else Console.Write($" - {readKey.KeyChar} / ");
+    }
+};
+
+EndInfor();
+
+#endregion
